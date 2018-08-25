cb820249314223da4943a5f19d013caaa6df7a3a
郑军 <zhengjun1987@outlook.com>
2018-08-25 22:59:11 +0800

    chapter02 data_structure

---
+++ chapter02data_structure/intro.py
@@ -0,0 +1,13 @@
+edward = ['Edward Gumby', 42]
+john = ['John Smith', 50]
+database = [edward, john]
+print database  # [['Edward Gumby', 42], ['John Smith', 50]]
+
+print database[1]  # ['John Smith', 50]
+
+greetings = 'hello'
+print greetings[0]  # h
+print greetings[0] * 5  # hhhhh
+
+# x = 42
+# print x[0]  #TypeError: 'int' object has no attribute '__getitem__'
