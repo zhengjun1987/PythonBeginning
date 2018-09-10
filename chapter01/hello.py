8fbe7f4c06cd895370dbb378c2cc24c7022d561c
郑军 <zhengjun1987@outlook.com>
2018-09-10 14:16:45 +0800

    第一章 基础知识

---
+++ chapter01/hello.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+print("Hello, world!")
+# zhengjuns-MacBook-Pro:PythonBeginning zhengjun$ python3 chapter01/hello.py
+# Hello, world!
+
+name = input("What is your name?")
+print("Hello, " + name + "!")
+# zhengjuns-MacBook-Pro:PythonBeginning zhengjun$ python3 chapter01/hello.py
+# What is your name?Zheng Jun
+# Hello, Zheng Jun!
+
+print("""zhengjuns-MacBook-Pro:
+PythonBeginning
+zhengjun$
+python3
+chapter01/hello.py""")
+# zhengjuns-MacBook-Pro:
+# PythonBeginning
+# zhengjun$
+# python3
+# chapter01/hello.py
+
+print("""She said, "Let's go to the muse".Then the sun went down.""")
+# She said, "Let's go to the muse".Then the sun went down.
